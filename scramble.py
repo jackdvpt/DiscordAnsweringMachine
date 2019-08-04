364359f8cdb373d72936ad1eed22ba8fcc0f59f8
jackdvpt <29292578+jackdvpt@users.noreply.github.com>
2019-08-04 17:32:39 +1000

    Update scramble.py

--- scramble.py
+++ scramble.py
@@ -12,12 +12,6 @@ from gtts.lang import tts_langs
 lang = {'de': 'german', 'uk': 'ukrainian', 'si': 'sinhala', 'vi': 'vietnamese', 'sq': 'albanian', 'km': 'khmer', 'mk': 'macedonian', 'pl': 'polish', 'la': 'latin', 'nl': 'dutch', 'lv': 'latvian', 'is': 'icelandic', 'el': 'greek', 'bs': 'bosnian', 'eo': 'esperanto', 'ca': 'catalan', 'en': 'english', 'ro': 'romanian', 'cs': 'czech', 'pt': 'portuguese',
         'zh-cn': 'chinese (simplified)', 'ko': 'korean', 'su': 'sundanese', 'fr': 'french', 'sr': 'serbian', 'hi': 'hindi', 'da': 'danish', 'af': 'afrikaans', 'te': 'telugu', 'ta': 'tamil', 'cy': 'welsh', 'sk': 'slovak', 'no': 'norwegian', 'hu': 'hungarian', 'id': 'indonesian', 'tr': 'turkish', 'th': 'thai', 'sw': 'swahili', 'es': 'spanish', 'ja': 'japanese', 'my': 'myanmar (burmese)', 'tl': 'filipino', 'et': 'estonian', 'fi': 'finnish', 'zh-tw': 'chinese (traditional)', 'it': 'italian', 'ru': 'russian', 'ar': 'arabic', 'hy': 'armenian', 'sv': 'swedish', 'hr': 'croatian', 'mr': 'marathi', 'bn': 'bengali', 'ne': 'nepali', 'ml': 'malayalam', 'jw': 'javanese'}
 
-def randomise(string, who):
-    language = random.choice(list(lang))
-    translator = Translator()
-    print(lang[language])
-    play(translator.translate(string, dest=language).text,who, language)
-
 
 def roundtranslate(string, who):
     langs = set()
@@ -37,8 +31,7 @@ def roundtranslate(string, who):
 def translate(string, who):
     translator = Translator()
     language = random.choice(list(lang))
-    translated = translator.translate(string, dest=language).text
-    talk.play(translated, who)
+    talk.play(translator.translate(string, dest=language).text, who)
     return language
 
 
